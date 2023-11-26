e83bfcf39bb8934c2beb6963767fa6ac72da303e
ArielGC00 <josgomez@estudiantec.cr>
2023-11-25 19:58:03 -0600

    Actualización al RSA

--- src/main/java/controlador/CifradoRSAController.java
+++ src/main/java/controlador/CifradoRSAController.java
@@ -1,8 +1,12 @@
 
 package controlador;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import logicadenegocios.CifradoRSA;
 import vistas.Interfaz;
 import java.math.BigInteger;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Ariel Gomez y Marco Perez
@@ -30,12 +34,40 @@ public class CifradoRSAController{
             d=cifrador.getD();
             n=cifrador.getN();
             System.out.println("Cifrado--- "+"D:"+d+" N:"+n);
+            mostrarMensajeConCopiaAlPortapapeles(vista, "Clave 1: " + n);
+            mostrarMensajeConCopiaAlPortapapeles(vista, "Clave 2: " + d);
             vista.getSalidaText().setText(mensajeCifrado);
         } else {
-            System.out.println("Mensaje original: "+mensajeOriginal);
-            System.out.println("D:"+d+" N:"+n);
-            String mensajeDecodificado = cifrador.descifrar(mensajeOriginal,d,n);
+            try {
+            // Si el tipo de cifrado es "Llave", obtener la clave del usuario
+            String strClaveN = JOptionPane.showInputDialog(vista, "Ingrese la clave 1 para descifrar");
+            String strClaveD = JOptionPane.showInputDialog(vista, "Ingrese la clave 2 para descifrar");
+
+            // Convertir las cadenas a BigInteger
+            BigInteger claveN = new BigInteger(strClaveN);
+            BigInteger claveD = new BigInteger(strClaveD);
+
+            System.out.println("Mensaje original: " + mensajeOriginal);
+            System.out.println("D:" + d + " N:" + n);
+
+            String mensajeDecodificado = cifrador.descifrar(mensajeOriginal, claveD, claveN);
             vista.getSalidaText().setText(mensajeDecodificado);
+            } catch (NumberFormatException e) {
+                // Capturar la excepción si la entrada del usuario no es un número válido
+                JOptionPane.showMessageDialog(vista, "Error: Ingrese números válidos para las claves.");
+                vista.getSalidaText().setText(" ");
+            }
         }
     }
+    private void mostrarMensajeConCopiaAlPortapapeles(Interfaz vista, String mensaje) {
+        // Copiar al portapapeles
+        StringSelection stringSelection = new StringSelection(mensaje);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+
+        // Mostrar el cuadro de diálogo con la opción de copiar al portapapeles
+        JOptionPane.showMessageDialog(vista, mensaje, "Clave", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+        
 }
